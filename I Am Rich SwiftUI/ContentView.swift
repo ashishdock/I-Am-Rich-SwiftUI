6b2f193acada34ff4a8a164eb489be0feedd8315
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-16 13:22:27 +0530

    Started the project

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Ashish Sharma on 12/16/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, earth!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
